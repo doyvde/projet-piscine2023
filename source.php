27e40bd8715a6453971e899169c84f23a7de69b3
doyvde <95855418+doyvde@users.noreply.github.com>
2023-06-04 17:17:49 +0200

    source

---
+++ source.php
@@ -0,0 +1,14 @@
+<?php 
+        
+        //source:https://getbootstrap.com/docs/4.0/components/dropdowns/
+        //source:https://mdbootstrap.com/docs/standard/extended/credit-card/
+        //source:https://gosnippets.com/snippets/bootstrap-carousel-with-cards-in-3-columns
+         //source:https://www.hostinger.fr/tutoriels/favicon
+        //source:https://github.com/mathieudad/Ece-Ebay
+        //source:https://getbootstrap.com/docs/4.0/components/card/
+        //source:https://www.wsupercars.com/
+        //source:https://stackoverflow.com/questions/12328354/calling-a-particular-php-function-on-form-submit
+        //source:https://getbootstrap.com/docs/4.0/components/collapse/ 
+        //source:https://getbootstrap.com/docs/4.0/components/carousel/
+        
+?>
